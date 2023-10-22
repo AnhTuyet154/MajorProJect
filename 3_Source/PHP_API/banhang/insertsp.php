93ec971d8c806d80bbf5bb5ac8eb8111142f7db9
Anh Tuyet <ttuyet1504@gmail.com>
2023-10-22 12:22:19 +0700

    Api PHP all

--- 3_Source/PHP_API/banhang/insertsp.php
+++ 3_Source/PHP_API/banhang/insertsp.php
@@ -5,8 +5,11 @@ $gia = $_POST['gia'];
 $hinhanh = $_POST['hinhanh'];
 $mota = $_POST['mota'];
 $loai = $_POST['loai'];
+//bai66
+//$sltonkho = $_POST['slsp'];
 
-$query = 'INSERT INTO `sanphammoi`( `tensp`, `giasp`, `hinhanh`, `mota`, `loai`) VALUES ("'.$tensp.'","'.$gia.'","'.$hinhanh.'","'.$mota.'",'.$loai.')';
+$query = 'INSERT INTO `sanphammoi`( `tensp`, `giasp`, `hinhanh`, `mota`, `loai`, `sltonkho`) VALUES ("'.$tensp.'","'.$gia.'","'.$hinhanh.'","'.$mota.'",'.$loai.')';
+//$query = 'INSERT INTO `sanphammoi`( `tensp`, `giasp`, `hinhanh`, `mota`, `loai`, `sltonkho`) VALUES ("'.$tensp.'","'.$gia.'","'.$hinhanh.'","'.$mota.'",'.$loai.','.$sltonkho.')';
 $data = mysqli_query($conn, $query);
 
 if($data == true){
